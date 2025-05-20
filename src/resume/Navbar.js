0dfd41eb69c5b4da033a765c5901f91065f0d8a4
Yuliya Semakhina <47063920+JuliaSemakhina@users.noreply.github.com>
2025-05-20 21:15:53 +0300

    updated design files

--- src/resume/Navbar.js
+++ src/resume/Navbar.js
@@ -49,38 +49,3 @@ const Navbar = () => {
 
 export default Navbar
 
-// const Navbar = () => {
-//   const { openSidebar, toggleTheme } = useGlobalContext();
-
-//   return (
-//     <nav className='nav d-f_jc-c'>
-//       <div className='nav-center'>
-//         <div className='nav-header'>
-//           <img src={logo2} alt='stripe' className='nav-logo' />
-//           <CgDarkMode className='theme-icon' onClick={toggleTheme} />
-//           <button className='btn toggle-btn' onClick={openSidebar}>
-//             <FaBars />
-//           </button>
-//         </div>
-//         <ul className='nav-links'>
-//           <li>
-//             <a href='#projects' className='link-btn'><span>/</span>projects</a>
-//           </li>
-//           <li>
-//             <a href='#skills' className='link-btn'><span>/</span>skills</a>
-//           </li>
-//         </ul>
-//         <div className='link-socials d-f_jc-c'>
-//           <a href='#skills' className='link-btn'><span>/</span>education</a>
-//           <a href='#contacts' className='link-btn'><span>/</span>contacts</a>
-//         </div>
-//         <div className="switch-btn" id="_2nd-toggle-btn" onClick={toggleTheme}>
-//           <input type="checkbox" id="theme-toggle" />
-//           <label htmlFor="theme-toggle"></label>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
